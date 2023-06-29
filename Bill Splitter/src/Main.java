a93957b699e1d538461f5b1c47e7765a67c6b863
Runjeet <runjeetnarula@berkeley.edu>
2023-06-28 17:01:00 -0700

    added pseudocode comments

--- Bill Splitter/src/Main.java
+++ Bill Splitter/src/Main.java
@@ -3,8 +3,23 @@ import java.util.*;
 public class Main {
     Scanner scan = new Scanner(System.in);
     List<Friend> friends = new ArrayList<>();
+    List<Food> foods = new ArrayList<>();
 
     public static void main(String[] args){
-        
+
+    }
+    public void addFriends(){
+        // continuously check for new input in a while loop:
+            // if the name isn't already in the list, add it
+            // if it is, ask if it's another friend with the same name or a mistake
+                // if it's a diff person, ask for last initial and append that to their name
+    }
+    public void addFoods(){
+        // continuously check for new input
+            // if entered item (name) is not already in the list, create a food object and add it
+            // else update the food's price with the new price
+    }
+    public void print(){
+        // print the names of all friends who don't owe money, how much they owe, and what they've eaten
     }
 }
